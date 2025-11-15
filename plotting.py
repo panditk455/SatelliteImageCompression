5cc2428e9ca38dc16c86c1d93aead0c45d74e663
Anika Rajbhandary <126826820+rajbhandarya@users.noreply.github.com>
2025-11-14 23:39:51 -0600

    Create Plotting.py

---
+++ plotting.py
@@ -0,0 +1,144 @@
+"""
+Plotting functions for visualizing results from compression experiments.
+Note: these functions read from specific CSV files generated during the experiments.
+Should generalize to be run on a singular results csv in future as needed.
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+new_dir = 'Plots'
+if not os.path.exists(new_dir):
+    os.mkdir(new_dir)
+
+# Heatmaps for representing spatial data, frequency data, and quantized frequency data ----------------
+pixel_block = np.array([
+    [52.0, 139.0, 162.0, 116.0, 97.0, 107.0, 107.0, 108.0],
+    [89.0, 153.0, 163.0, 127.0, 110.0, 114.0, 118.0, 127.0],
+    [139.0, 173.0, 165.0, 142.0, 130.0, 124.0, 120.0, 127.0],
+    [179.0, 187.0, 169.0, 145.0, 120.0, 102.0, 109.0, 133.0],
+    [191.0, 192.0, 170.0, 123.0, 84.0, 74.0, 99.0, 140.0],
+    [190.0, 186.0, 157.0, 95.0, 69.0, 81.0, 105.0, 139.0],
+    [189.0, 170.0, 131.0, 82.0, 73.0, 94.0, 113.0, 143.0],
+    [179.0, 136.0, 88.0, 59.0, 60.0, 88.0, 132.0, 161.0]
+])
+
+dct_coefficients = np.array([
+    [-5.8, 132.6, 123.7, -83.2, -38.8, -26.3, 3.4, -1.5],
+    [10.4, -36.2, -144.4, -45.6, -38.6, -15.5, -6.4, -2.6],
+    [-79.5, -82.8, -1.4, 10.5, -25.3, -1.0, 0.7, 0.8],
+    [-26.4, 27.0, 2.7, -32.0, -19.8, -3.9, 7.3, 1.5],
+    [-16.2, -2.8, 6.1, -5.5, 10.2, 5.3, -3.6, 0.5],
+    [11.3, 9.0, -15.6, 9.7, -1.8, -3.5, 1.4, -1.0],
+    [-5.0, -5.0, 0.7, 4.5, -3.6, 1.1, -0.6, 0.7],
+    [1.0, 3.6, -2.9, -0.5, -0.5, -1.1, 0.4, -0.7]
+])
+
+quantization_matrix = np.array([
+    [16, 11, 10, 16, 24, 40, 51, 61],
+    [12, 12, 14, 19, 26, 58, 60, 55],
+    [14, 13, 16, 24, 40, 57, 69, 56],
+    [14, 17, 22, 29, 51, 87, 80, 62],
+    [18, 22, 37, 56, 68, 109, 103, 77],
+    [24, 35, 55, 64, 81, 104, 113, 92],
+    [49, 64, 78, 87, 103, 121, 120, 101],
+    [72, 92, 95, 98, 112, 100, 103, 99]
+])
+
+def plot_heatmap(data: np.ndarray, title: str, save_path: str):
+    plt.figure(figsize=(10, 6))
+    plt.imshow(data, cmap='gray', interpolation='nearest')
+    plt.colorbar()
+    plt.title(title)
+    plt.tight_layout()
+    plt.savefig(save_path)
+    plt.close()
+
+# Rate-Distortion Curve for Quality Experiments -------------------------
+def rate_distortion_curve(csv_path: str, save_path: str):
+    df = pd.read_csv(csv_path)
+    unique_images = df['image_name'].unique()
+
+    plt.figure(figsize=(10, 6))
+
+    for image in unique_images:
+        image_df = df[df['image_name'] == image]
+        image_df = image_df.sort_values(by='bpp')
+        plt.plot(image_df['bpp'], image_df['mse'], label=image, marker='o')
+
+    plt.xlabel('Rate (Bits Per Pixel - bpp)')
+    plt.ylabel('Distortion (Mean Squared Error - MSE)')
+    plt.title('Rate-Distortion Curve (MSE vs. bpp)')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.savefig(save_path)
+    plt.close()
+
+# Comparing the Quantization methods ---------------------------------
+
+def quantization_comparison(csvs: list[str]):
+    dfs = [pd.read_csv(csv) for csv in csvs]
+    unique_methods = pd.concat(dfs)['quantization_method'].unique()
+
+    plt.figure(figsize=(10, 6))
+    counter = 0
+    for method in unique_methods:
+        method_df = pd.concat(dfs)[pd.concat(dfs)['quantization_method'] == method]
+
+        method_df = method_df.sort_values(by='bpp')
+        plt.plot(method_df['bpp'], method_df['psnr_db'], label=method, marker='o')
+        plt.xlabel('Rate (Bits Per Pixel - bpp)')
+        plt.ylabel('Distortion (Peak Signal-to-Noise Ratio - PSNR)')
+        plt.title(f'Rate-Distortion Curve of {method_df["image_name"].iloc[0]} for Different Quantization Methods')
+
+        plt.legend()
+        plt.grid(True)
+
+        plt.tight_layout()
+
+        image_path = os.path.join(new_dir, f'quantization_distortion_curve{counter}.png')
+        plt.savefig(image_path)
+
+        plt.show()
+        counter += 1
+
+
+# BPP vs Spatial Complexity at fixed quality level 75 -----------------------
+def bpp_vs_spatial_complexity(csv_path: str):
+    df = pd.read_csv(csv_path)
+    df_quality_75 = df[df['quality_setting'] == 75]
+
+    plt.figure(figsize=(10, 6))
+
+    x = df_quality_75['edge_density']
+    y = df_quality_75['bpp']
+
+    plt.scatter(x, y)
+
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+
+    #add trendline to plot
+    plt.plot(x, p(x))
+
+    plt.xlabel('Edge Density')
+    plt.ylabel('Rate (Bits Per Pixel - bpp)')
+    plt.title('BPP vs. Edge Density (Quality = 75)')
+
+    plt.grid(True)
+    plt.tight_layout()
+
+    image_path = os.path.join(new_dir, 'edge_density_vs_bpp.png')
+    plt.savefig(image_path)
+
+    plt.show()
+
+
+
+def main():
+    plot_heatmap(pixel_block, 'Spatial Domain Heatmap', os.path.join(new_dir, 'spatial_heatmap.png'))
+    plot_heatmap(dct_coefficients, 'Frequency Domain Heatmap (DCT Coefficients)', os.path.join(new_dir, 'frequency_heatmap.png'))
+    plot_heatmap(np.round(dct_coefficients / quantization_matrix).astype(int), 'Quantized Heatmap', os.path.join(new_dir, 'quantized_frequency_heatmap.png'))
